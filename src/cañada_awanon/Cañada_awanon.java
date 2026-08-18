4ab303cf81648b78d5b43cff523192612adfcf70
unknown <biencicanada6@gmail.com>
2026-08-18 10:46:11 +0800

    my first commit

---
+++ src/cañada_awanon/Cañada_awanon.java
@@ -0,0 +1,12 @@
+/
+package cañada_awanon;
+
+
+public class Cañada_awanon {
+
+   
+    public static void main(String[] args) {
+       System.out.println("Welcom Welcome Mga idol");
+    }
+    
+}
